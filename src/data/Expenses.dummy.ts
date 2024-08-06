c263068caa949e073d09420b706f4cf57f68f05d
Emre Portakal <emre.portakal@commencis.com>
2024-08-06 09:22:45 +0300

    chore: add dummy data

---
+++ src/data/Expenses.dummy.ts
@@ -0,0 +1,28 @@
+const DUMMY_EXPENSES = [
+  {
+    id: "1",
+    title: "Rent",
+    amount: 856.47,
+    date: "2024-07-06T21:12:07.304Z",
+  },
+  {
+    id: "2",
+    title: "Groceries",
+    amount: 123.54,
+    date: "2024-07-12T21:12:07.304Z",
+  },
+  {
+    id: "3",
+    title: "Utilities",
+    amount: 432.89,
+    date: "2024-07-30T21:12:07.304Z",
+  },
+  {
+    id: "4",
+    title: "Transportation",
+    amount: 78.45,
+    date: "2024-07-21T21:12:07.304Z",
+  },
+];
+
+export default DUMMY_EXPENSES;
